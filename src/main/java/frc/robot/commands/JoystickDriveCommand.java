cc5053a4b19a89e3da68b7dfa3d3eb1030021dad
yteam3211 <yer.3211@gmail.com>
2021-05-06 20:47:57 +0300

    Next commit

---
+++ src/main/java/frc/robot/commands/JoystickDriveCommand.java
@@ -0,0 +1,37 @@
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveTrain;
+
+public class JoystickDriveCommand extends CommandBase {
+
+  double leftStick, rightStick;
+  double leftOutput, rightOutput;
+
+  Joystick joystick;
+  DriveTrain driveTrain;
+
+  public JoystickDriveCommand(Joystick joystick, DriveTrain driveTrain) {
+    this.joystick = joystick;
+    this.driveTrain = driveTrain;
+    addRequirements(driveTrain);
+  }
+
+  @Override
+  public void initialize() {}
+
+  @Override
+  public void execute() {
+    driveTrain.set
+  }
+
+  @Override
+  public void end(boolean interrupted) {}
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
